9d04ba670f7f96a93e5eb2f8490cc2016fbc70e2
Jason Engstrom <engstrom.jason@gmail.com>
2017-07-04 18:52:29 -0400

    composed spec - parameter replacement
    Fixes invalid op exception when composing specs.

---
+++ SpecificationPattern/ParameterReplacer.cs
@@ -0,0 +1,16 @@
+﻿using System.Linq.Expressions;
+
+namespace SpecificationPattern {
+
+    internal class ParameterReplacer : ExpressionVisitor {
+
+        private readonly ParameterExpression _parameter;
+
+        protected override Expression VisitParameter(ParameterExpression node) 
+            => base.VisitParameter(_parameter);
+
+        internal ParameterReplacer(ParameterExpression parameter) {
+            _parameter = parameter;
+        }
+    }
+}
--- SpecificationPattern/Specifications.cs
+++ SpecificationPattern/Specifications.cs
@@ -1,81 +1,76 @@
 ﻿using System;
-using System.Linq;
 using System.Linq.Expressions;
 
 
-namespace SpecificationPattern
-{
-    public abstract class Specification<T>
-    {
+namespace SpecificationPattern {
+
+    public abstract class Specification<T> {
+
         public abstract Expression<Func<T, bool>> ToExpression();
 
 
-        public bool IsSatisfiedBy(T entity)
-        {
+        public bool IsSatisfiedBy(T entity) {
             Func<T, bool> predicate = ToExpression().Compile();
             return predicate(entity);
         }
 
 
-        public Specification<T> And(Specification<T> specification)
-        {
+        public Specification<T> And(Specification<T> specification) {
             return new AndSpecification<T>(this, specification);
         }
 
 
-        public Specification<T> Or(Specification<T> specification)
-        {
+        public Specification<T> Or(Specification<T> specification) {
             return new OrSpecification<T>(this, specification);
         }
     }
 
 
-    public class AndSpecification<T> : Specification<T>
-    {
+    public class AndSpecification<T> : Specification<T> {
         private readonly Specification<T> _left;
         private readonly Specification<T> _right;
 
 
-        public AndSpecification(Specification<T> left, Specification<T> right)
-        {
+        public AndSpecification(Specification<T> left, Specification<T> right) {
             _right = right;
             _left = left;
         }
 
 
-        public override Expression<Func<T, bool>> ToExpression()
-        {
+        public override Expression<Func<T, bool>> ToExpression() {
             Expression<Func<T, bool>> leftExpression = _left.ToExpression();
             Expression<Func<T, bool>> rightExpression = _right.ToExpression();
 
-            BinaryExpression andExpression = Expression.AndAlso(leftExpression.Body, rightExpression.Body);
+            var paramExpr = Expression.Parameter(typeof(T));
+            var exprBody = Expression.AndAlso(leftExpression.Body, rightExpression.Body);
+            exprBody = (BinaryExpression)new ParameterReplacer(paramExpr).Visit(exprBody);
+            var finalExpr = Expression.Lambda<Func<T, bool>>(exprBody, paramExpr);
 
-            return Expression.Lambda<Func<T, bool>>(andExpression, leftExpression.Parameters.Single());
+            return finalExpr;
         }
     }
 
 
-    public class OrSpecification<T> : Specification<T>
-    {
+    public class OrSpecification<T> : Specification<T> {
         private readonly Specification<T> _left;
         private readonly Specification<T> _right;
 
 
-        public OrSpecification(Specification<T> left, Specification<T> right)
-        {
+        public OrSpecification(Specification<T> left, Specification<T> right) {
             _right = right;
             _left = left;
         }
 
 
-        public override Expression<Func<T, bool>> ToExpression()
-        {
-            Expression<Func<T, bool>> leftExpression = _left.ToExpression();
-            Expression<Func<T, bool>> rightExpression = _right.ToExpression();
+        public override Expression<Func<T, bool>> ToExpression() {
+            var leftExpression = _left.ToExpression();
+            var rightExpression = _right.ToExpression();
+            var paramExpr = Expression.Parameter(typeof(T));
+            var exprBody = Expression.OrElse(leftExpression.Body, rightExpression.Body);
+            exprBody = (BinaryExpression)new ParameterReplacer(paramExpr).Visit(exprBody);
+            var finalExpr = Expression.Lambda<Func<T, bool>>(exprBody, paramExpr);
 
-            BinaryExpression andExpression = Expression.OrElse(leftExpression.Body, rightExpression.Body);
-
-            return Expression.Lambda<Func<T, bool>>(andExpression, leftExpression.Parameters.Single());
+            return finalExpr;
         }
     }
 }
